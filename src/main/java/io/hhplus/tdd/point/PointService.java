c2b457952b3dade8625d767c6c1fb1901aa6fde3
HyeJin Ryoo <56506753+hjryoo@users.noreply.github.com>
2025-08-11 21:17:54 +0900

    포인트 조회하는 기능 작성

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -13,4 +13,15 @@ public class PointService {
         this.userPointTable = userPointTable;
         this.pointHistoryTable = pointHistoryTable;
     }
+
+    /**
+     * 특정 사용자의 포인트를 조회한다.
+     *
+     * @param userId 사용자 ID
+     * @return 사용자의 현재 포인트 정보
+     */
+    public UserPoint getUserPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
+
 }
